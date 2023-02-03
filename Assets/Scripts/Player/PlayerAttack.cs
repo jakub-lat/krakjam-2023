f9e62e8b9554ee3e0e02016fa2b359fbed51b468
Jakub L <jakub@cubepotato.eu>
2023-02-04 00:48:01 +0100

    player attack

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -6,6 +6,12 @@ namespace Player
 {
     public class PlayerAttack : MonoSingleton<PlayerAttack>
     {
+        [SerializeField] private float attackDamage = 20f;
+        [SerializeField] private new Collider2D collider;
+        [SerializeField] private LayerMask enemyLayerMask;
+
+        private Collider2D[] results = { };
+
         private void Update()
         {
             Attack();
@@ -14,8 +20,18 @@ namespace Player
         private void Attack()
         {
             if (!Input.GetMouseButtonDown(0)) return;
-            
-            
+
+            results = new Collider2D[] { };
+            var res = Physics2D.OverlapBox(transform.position,
+                new Vector2(collider.bounds.size.x * transform.lossyScale.x,
+                    collider.bounds.size.y * transform.lossyScale.y), transform.rotation.eulerAngles.z,
+                enemyLayerMask);
+            if (res)
+            {
+                var enemy = res.GetComponent<Enemy>();
+                enemy ??= res.GetComponentInParent<Enemy>();
+                enemy.GotHit(attackDamage);
+            }
         }
     }
 }
