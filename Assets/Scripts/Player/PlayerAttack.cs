e07fb7171203cd53dd751a46348ce668b57b9a4b
Jakub L <jakub@cubepotato.eu>
2023-02-04 00:48:15 +0100

    ++

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -9,9 +9,7 @@ namespace Player
         [SerializeField] private float attackDamage = 20f;
         [SerializeField] private new Collider2D collider;
         [SerializeField] private LayerMask enemyLayerMask;
-
-        private Collider2D[] results = { };
-
+        
         private void Update()
         {
             Attack();
@@ -21,7 +19,6 @@ namespace Player
         {
             if (!Input.GetMouseButtonDown(0)) return;
 
-            results = new Collider2D[] { };
             var res = Physics2D.OverlapBox(transform.position,
                 new Vector2(collider.bounds.size.x * transform.lossyScale.x,
                     collider.bounds.size.y * transform.lossyScale.y), transform.rotation.eulerAngles.z,
