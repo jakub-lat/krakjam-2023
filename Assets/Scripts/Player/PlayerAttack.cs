e98e96e78a82a33d771d699e08bbf3939c85fb05
Jakub L <jakub@cubepotato.eu>
2023-02-04 00:05:16 +0100

    player flip

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using Utils;
+
+namespace Player
+{
+    public class PlayerAttack : MonoSingleton<PlayerAttack>
+    {
+        private void Update()
+        {
+            Attack();
+        }
+
+        private void Attack()
+        {
+            if (!Input.GetMouseButtonDown(0)) return;
+            
+            
+        }
+    }
+}
