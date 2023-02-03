980676f5d460703c58ec058862071166c7e9f5f9
Jakub L <jakub@cubepotato.eu>
2023-02-03 22:12:07 +0100

    better PlayerMovement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Utils;
+
+namespace Player
+{
+    public class PlayerMovement : MonoSingleton<PlayerMovement>
+    {
+        [SerializeField] private float moveSpeed = 10f;
+        [SerializeField] private float jumpForce = 10f;
+        [SerializeField] private LayerMask groundMask;
+        [SerializeField] private Transform groundCheck;
+
+        private Rigidbody2D rb;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            Move();
+            Jump();
+        }
+
+        private void Move()
+        {
+            var horizontal = Input.GetAxis("Horizontal");
+            var move = new Vector2(horizontal * moveSpeed * Time.deltaTime, 0);
+            transform.Translate(move);
+        }
+
+        private void Jump()
+        {
+            if (!Input.GetKeyDown(KeyCode.W)) return;
+            
+            var isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundMask);
+            if (!isGrounded) return;
+            
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+        }
+    }
+}
