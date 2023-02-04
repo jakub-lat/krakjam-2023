38e4b0b9d3902a1e7da1f8235e0c2f766d5cf20f
Jakub L <jakub@cubepotato.eu>
2023-02-04 10:19:55 +0100

    scene, smooth camera

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using Utils;
+using Cinemachine;
 
 namespace Player
 {
@@ -12,12 +13,20 @@ namespace Player
         [SerializeField] private LayerMask groundMask;
         [SerializeField] private Transform groundCheck;
         [SerializeField] private Transform playerBody;
-
+        
         private Rigidbody2D rb;
+
+        private CinemachineTransposer vCamTransposer;
+
+        private float lerpT;
+        private float lerpFrom;
+        private float lerpTo;
+        private bool isLerping;
 
         private void Start()
         {
             rb = GetComponent<Rigidbody2D>();
+            vCamTransposer = VCamInstance.Current.GetCinemachineComponent<CinemachineTransposer>();
         }
 
         private void Update()
@@ -36,6 +45,24 @@ namespace Player
             {
                 playerBody.localRotation = Quaternion.Euler(new Vector3(playerBody.localRotation.eulerAngles.x,
                     horizontal > 0 ? 0 : 180, playerBody.localRotation.eulerAngles.z));
+
+                if (!isLerping)
+                {
+                    lerpFrom = vCamTransposer.m_FollowOffset.x;
+                    lerpTo = Mathf.Sign(horizontal) * 3.5f;
+                    lerpT = 0;
+                    isLerping = true;
+                }
+                
+                var offset = vCamTransposer.m_FollowOffset;
+                offset.x = Mathf.Lerp(lerpFrom, lerpTo, Mathf.SmoothStep(0, 1, lerpT));
+                vCamTransposer.m_FollowOffset = offset;
+
+                lerpT += Time.deltaTime * 1.2f;
+            }
+            else
+            {
+                isLerping = false;
             }
         }
 
---
+++ Assets/Scripts/VCamInstance.cs
@@ -0,0 +1,12 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class VCamInstance : MonoBehaviour
+{
+    public static CinemachineVirtualCamera Current { get; private set; }
+
+    private void Awake()
+    {
+        Current = GetComponent<CinemachineVirtualCamera>();
+    }
+}
