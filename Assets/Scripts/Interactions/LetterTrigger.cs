668b6b8c954fdf5a0001b0e19a160c0f9677008a
jatoja <fikulatu@gmail.com>
2023-02-05 04:52:49 +0100

    Letters engls

--- Assets/Scripts/Interactions/LetterTrigger.cs
+++ Assets/Scripts/Interactions/LetterTrigger.cs
@@ -7,12 +7,15 @@ namespace Interactions
     public class LetterTrigger : BaseInteraction
     {
         [SerializeField, TextArea] private string content;
+        [SerializeField] private AudioClip letterAudio;
+        [SerializeField] private AudioSource source;
 
         protected override string InteractionText => "Read letter";
 
         protected override void Interact()
         {
             LetterController.Current.Show(content);
+            if(letterAudio!=null) {source.PlayOneShot(letterAudio);}
         }
     }
 }
